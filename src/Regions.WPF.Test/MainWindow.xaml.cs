773911d7731e95fef22c48bea0e9bf90930bc3d2
ema <mccoy39082@163.com>
2025-08-14 14:56:15 +0800

    chore: test grid

--- src/Regions.WPF.Test/MainWindow.xaml.cs
+++ src/Regions.WPF.Test/MainWindow.xaml.cs
@@ -10,30 +10,37 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         _regionManager = RegionServiceProvider.GetRequiredService<IRegionManager>();
+        //_regionManager.RequestNavigate("MainRegion", new Uri("/PageA", UriKind.Relative));
+        _regionManager.RequestNavigate("MainGridRegion", new Uri("/GridA", UriKind.Relative));
     }
 
     private void BtnA_Click(object sender, RoutedEventArgs e)
     {
-        _regionManager?.RequestNavigate("MainRegion", new Uri("/PageA", UriKind.Relative));
+        //_regionManager?.RequestNavigate("MainRegion", new Uri("/PageA", UriKind.Relative));
+        _regionManager?.RequestNavigate("MainGridRegion", new Uri("/GridA", UriKind.Relative));
     }
 
     private void BtnB_Click(object sender, RoutedEventArgs e)
     {
-        _regionManager?.RequestNavigate("MainRegion", new Uri("/PageB", UriKind.Relative));
+        //_regionManager?.RequestNavigate("MainRegion", new Uri("/PageB", UriKind.Relative));
+        _regionManager?.RequestNavigate("MainGridRegion", new Uri("/GridB", UriKind.Relative));
     }
 
     private void BtnBack_Click(object sender, RoutedEventArgs e)
     {
-        _regionManager?.RequestGoBack("MainRegion");
+        //_regionManager?.RequestGoBack("MainRegion");
+        _regionManager?.RequestGoBack("MainGridRegion");
     }
 
     private void BtnForward_Click(object sender, RoutedEventArgs e)
     {
-        _regionManager?.RequestGoForward("MainRegion");
+        //_regionManager?.RequestGoForward("MainRegion");
+        _regionManager?.RequestGoForward("MainGridRegion");
     }
 
     private void BtnRedirect_Click(object sender, RoutedEventArgs e)
     {
-        _regionManager?.RequestRedirect("MainRegion", new Uri("/PageA", UriKind.Relative));
+        //_regionManager?.RequestRedirect("MainRegion", new Uri("/PageA", UriKind.Relative));
+        _regionManager?.RequestRedirect("MainGridRegion", new Uri("/GridA", UriKind.Relative));
     }
 }
